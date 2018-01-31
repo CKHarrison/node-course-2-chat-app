8e349e2df82f03d137bb92d73f4dc610f513e511
CKHarrison <hirohumi1882@gmail.com>
2018-01-30 17:53:00 -0800

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  // socket.emit to user who joined, from Admin, text "welcome to the chat app"
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the app!'
+  });
+
+  // socket.broadcast.emit from admin text, new user joined
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined!'
+  });
 
   socket.on('createMessage', (message) => {
     console.log('received message from user: ', message);
@@ -21,8 +32,13 @@ io.on('connection', (socket) => {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
-    })
-  })
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   date: new Date().getTime()
+    // });
+  });
 
   socket.on('disconnect', () => {
     console.log('Client Disconnected');
